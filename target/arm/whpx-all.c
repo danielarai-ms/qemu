0435f5d5e92f52c3fbd672afebc7240853f86954
Daniel Arai <danielarai@microsoft.com>
2025-05-02 11:56:37 -0700

    Add empty whpx-all.c file and associated build update for arm host

---
+++ target/arm/whpx-all.c
@@ -0,0 +1,9 @@
+/*
+ * QEMU Windows Hypervisor Platform accelerator (WHPX)
+ *
+ * Copyright Microsoft Corp. 2025
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
