38b5ef696190494b751d3b97c5e836dd07af8fb7
Daniel Arai <danielarai@microsoft.com>
2025-05-12 16:09:03 -0700

    Finish reading CPU features and exposing them to QEMU

--- target/arm/whpx-all.c
+++ target/arm/whpx-all.c
@@ -53,11 +53,6 @@ struct WHPDispatch whp_dispatch;
  * to the appropriate WHP API.
  */
 static WHV_REGISTER_NAME whpx_isar_register_names[] = {
-    WHvRegisterHypervisorVersion,
-    WHvRegisterPrivilegesAndFeaturesInfo,
-    WHvRegisterFeaturesInfo,
-    WHvRegisterImplementationLimitsInfo,
-    WHvRegisterHardwareFeaturesInfo,
     WHvArm64RegisterIdIsar0El1,
     WHvArm64RegisterIdIsar1El1,
     WHvArm64RegisterIdIsar2El1,
@@ -65,11 +60,10 @@ static WHV_REGISTER_NAME whpx_isar_register_names[] = {
     WHvArm64RegisterIdIsar4El1,
     WHvArm64RegisterIdIsar5El1,
 
-    WHvArm64RegisterIdAa64Mmfr0El1,
-    WHvArm64RegisterIdAa64Mmfr1El1,
-    WHvArm64RegisterIdAa64Mmfr2El1,
-    WHvArm64RegisterIdAa64Mmfr3El1,
-    WHvArm64RegisterIdAa64Mmfr4El1,
+    WHvArm64RegisterIdMmfr0El1,
+    WHvArm64RegisterIdMmfr1El1,
+    WHvArm64RegisterIdMmfr2El1,
+    WHvArm64RegisterIdMmfr3El1,
 
     WHvArm64RegisterIdPfr0El1,
     WHvArm64RegisterIdPfr1El1,
@@ -78,15 +72,13 @@ static WHV_REGISTER_NAME whpx_isar_register_names[] = {
     WHvArm64RegisterIdMvfr1El1,
     WHvArm64RegisterIdMvfr2El1,
 
-    WHvArm64RegisterIdAa64Dfr0El1,
-    WHvArm64RegisterIdAa64Dfr1El1,
+    WHvArm64RegisterIdDfr0El1,
+
+    /* TODO: dbgdidr, dbgdevid, dbgdevid1 */
 
     WHvArm64RegisterIdAa64Isar0El1,
     WHvArm64RegisterIdAa64Isar1El1,
     WHvArm64RegisterIdAa64Isar2El1,
-
-
-    /* TODO: dbgdidr, dbgdevid, dbgdevid1 */
 
     WHvArm64RegisterIdAa64Pfr0El1,
     WHvArm64RegisterIdAa64Pfr1El1,
@@ -95,7 +87,6 @@ static WHV_REGISTER_NAME whpx_isar_register_names[] = {
     WHvArm64RegisterIdAa64Mmfr1El1,
     WHvArm64RegisterIdAa64Mmfr2El1,
     WHvArm64RegisterIdAa64Mmfr3El1,
-    WHvArm64RegisterIdAa64Mmfr4El1,
 
     WHvArm64RegisterIdAa64Dfr0El1,
     WHvArm64RegisterIdAa64Dfr1El1,
@@ -103,16 +94,18 @@ static WHV_REGISTER_NAME whpx_isar_register_names[] = {
     WHvArm64RegisterIdAa64Zfr0El1,
     WHvArm64RegisterIdAa64Smfr0El1,
 
-    /* TODO: WHvArm64RegisterPmcrEl0,*/
+    /* TODO: reset_pmcr_el0,*/
 };
 
-static bool whpx_arm_get_cpu_features_from_host(void)
+static bool whpx_arm_get_cpu_features_from_host(ARMCPU *cpu)
 {
     struct whpx_state *whpx = &whpx_global;
     HRESULT hr;
     /* TODO: Is there an existing macro to get this size? */
     uint32_t register_count = sizeof(whpx_isar_register_names) / sizeof(WHV_REGISTER_NAME);
     WHV_REGISTER_VALUE isar_values[sizeof(whpx_isar_register_names) / sizeof(WHV_REGISTER_NAME)];
+    struct ARMISARegisters *isar = &cpu->isar;
+    WHV_REGISTER_VALUE *cur_isar_value;
 
     /* TODO: what's the indentation style? */
     hr = whp_dispatch.WHvGetVirtualProcessorRegisters(
@@ -124,13 +117,67 @@ static bool whpx_arm_get_cpu_features_from_host(void)
         return false;
     }
 
-    /* TODO: Just for debugging to see if we got something reasonable. */
-    int i;
-    for (i = 0; i < register_count; i++) {
-        printf("ISAR register %d: %08llx\n", i, isar_values[i].Reg64);
-    }
 
-    /* TODO: Need to convert the array of WHV_REGISTER_VALUES into the QEMU struct values. */
+    /* TODO: These assignments need to be kept in sync with the order of
+     * register names in whpx_isar_register_names. There's probably a better
+     * way to structure this that would be easier to maintain.
+     */
+    memset(isar, 0, sizeof(ARMISARegisters));
+
+    cur_isar_value = &isar_values[0];
+
+    /* TODO: Verify these values against the raw array values to make sure
+     * the assignment code is doing what I think it's doing.
+     */
+    isar->id_isar0 = (cur_isar_value++)->Reg32;
+    isar->id_isar1 = (cur_isar_value++)->Reg32;
+    isar->id_isar2 = (cur_isar_value++)->Reg32;
+    isar->id_isar3 = (cur_isar_value++)->Reg32;
+    isar->id_isar4 = (cur_isar_value++)->Reg32;
+    isar->id_isar5 = (cur_isar_value++)->Reg32;
+    /* TODO: isar->id_isar6 */
+
+    isar->id_mmfr0 = (cur_isar_value++)->Reg32;
+    isar->id_mmfr1 = (cur_isar_value++)->Reg32;
+    isar->id_mmfr2 = (cur_isar_value++)->Reg32;
+    isar->id_mmfr3 = (cur_isar_value++)->Reg32;
+    /* TODO: isar->id_mmfr4 */
+    /* TODO: isar->id_mmfr5 */
+
+    isar->id_pfr0 = (cur_isar_value++)->Reg32;
+    isar->id_pfr1 = (cur_isar_value++)->Reg32;
+    /* TODO: isar->id_pfr2 */
+
+    isar->mvfr0 = (cur_isar_value++)->Reg32;
+    isar->mvfr1 = (cur_isar_value++)->Reg32;
+    isar->mvfr2 = (cur_isar_value++)->Reg32;
+
+    isar->id_dfr0 = (cur_isar_value++)->Reg32;
+    /* TODO: isar->id_dfr1 */
+
+    isar->id_aa64isar0 = (cur_isar_value++)->Reg64;
+    isar->id_aa64isar1 = (cur_isar_value++)->Reg64;
+    isar->id_aa64isar2 = (cur_isar_value++)->Reg64;
+
+    isar->id_aa64pfr0 = (cur_isar_value++)->Reg64;
+    isar->id_aa64pfr1 = (cur_isar_value++)->Reg64;
+
+    isar->id_aa64mmfr0 = (cur_isar_value++)->Reg64;
+    isar->id_aa64mmfr1 = (cur_isar_value++)->Reg64;
+    isar->id_aa64mmfr2 = (cur_isar_value++)->Reg64;
+    isar->id_aa64mmfr3 = (cur_isar_value++)->Reg64;
+
+    isar->id_aa64dfr0 = (cur_isar_value++)->Reg64;
+    isar->id_aa64dfr1 = (cur_isar_value++)->Reg64;
+
+    isar->id_aa64zfr0 = (cur_isar_value++)->Reg64;
+    isar->id_aa64smfr0 = (cur_isar_value++)->Reg64;
+
+    /* If this check fails, it's actually too late, because we've already
+     * accessed off the end of the array.
+     */
+    assert(cur_isar_value - isar_values == register_count);
+
     return true;
 }
 
@@ -187,15 +234,15 @@ void whpx_arm_set_cpu_features_from_host(ARMCPU *cpu)
      * GetVirtualProcessorRegisters, possibly after creating a dummy VP
      * like KVM does.
      */
-    /* TODO: We're not doing anything with the ISAR values read from the host yet. */
-    if (!whpx_arm_get_cpu_features_from_host()) {
+    if (!whpx_arm_get_cpu_features_from_host(cpu)) {
         error_report("WHPX: Unable to read host CPU features");
 
-        /* WE can't report this error yet, so flag that we need to in
+        /* We can't report this error yet, so flag that we need to in
          * arm_cpu_realizefn().
          */
         cpu->host_cpu_probe_failed = true;
     }
+
 }
 
 /* TODO: Refactor to share code with i386 if possible. */
