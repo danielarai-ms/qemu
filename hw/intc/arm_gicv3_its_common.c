45310cd319351aa9bc299348c52c7b81356ac2ba
Daniel Arai <danielarai@microsoft.com>
2025-06-24 10:51:13 -0700

    Hack to run fully emulated without ITS

--- hw/intc/arm_gicv3_its_common.c
+++ hw/intc/arm_gicv3_its_common.c
@@ -169,7 +169,9 @@ const char *its_class_name(void)
         /* TODO: Is this sufficient for disabling ITS? */
         return NULL;
     } else {
+        /* XXX try forcing ITS off for emulation */
+        return NULL;
         /* Software emulation based model */
-        return "arm-gicv3-its";
+        //return "arm-gicv3-its";
     }
 }
